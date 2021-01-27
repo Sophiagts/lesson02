a776ac67ee22529b94cf2b680a4669510d9ee4f2
sophia <sophiats@uw.edu>
2021-01-26 17:25:02 -0800

    added values and operators

---
+++ calculator.js
@@ -0,0 +1,13 @@
+let x = 2;
+let y = 5;
+let z = 9;
+let a = 12;
+
+console.log(x-1);
+console.log(y+3);
+console.log(z*5);
+console.log(a/4);
+console.log(x**2);
+console.log(a%5);
+console.log(-x);
+console.log(x*z);
